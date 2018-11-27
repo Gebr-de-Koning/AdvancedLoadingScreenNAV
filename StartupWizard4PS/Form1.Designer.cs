a1d74d9347db96db5acbb04fb71e01ec103a2182
johan Veen <johveen@hotmail.com>
2018-11-27 13:46:42 +0100

    Johan is klaar Yanoah gaat verder

--- StartupWizard4PS/Form1.Designer.cs
+++ StartupWizard4PS/Form1.Designer.cs
@@ -31,12 +31,27 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Opstartkeuze));
             this.TitleText = new System.Windows.Forms.Label();
             this.OndertitelText = new System.Windows.Forms.Label();
-            this.ServerComboBox = new System.Windows.Forms.ComboBox();
-            this.ServerLabel = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.NAVImage = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rBtnProductie = new System.Windows.Forms.RadioButton();
+            this.rBtnTest = new System.Windows.Forms.RadioButton();
+            this.rBtnOntwikkel = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbRole = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rBtnDev = new System.Windows.Forms.RadioButton();
+            this.rBtnNoExpert = new System.Windows.Forms.RadioButton();
+            this.rBtnConfig = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.cmbCompany = new System.Windows.Forms.ComboBox();
+            this.btnOk = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.NAVImage)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // TitleText
@@ -44,11 +59,11 @@
             this.TitleText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.TitleText.AutoSize = true;
-            this.TitleText.Font = new System.Drawing.Font("Arial Unicode MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleText.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TitleText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(37)))), ((int)(((byte)(70)))));
             this.TitleText.Location = new System.Drawing.Point(127, 22);
             this.TitleText.Name = "TitleText";
-            this.TitleText.Size = new System.Drawing.Size(255, 33);
+            this.TitleText.Size = new System.Drawing.Size(321, 36);
             this.TitleText.TabIndex = 1;
             this.TitleText.Text = "Advanced Loginscreen";
             // 
@@ -57,34 +72,13 @@
             this.OndertitelText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.OndertitelText.AutoSize = true;
-            this.OndertitelText.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OndertitelText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.OndertitelText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(37)))), ((int)(((byte)(70)))));
             this.OndertitelText.Location = new System.Drawing.Point(157, 58);
             this.OndertitelText.Name = "OndertitelText";
-            this.OndertitelText.Size = new System.Drawing.Size(185, 21);
+            this.OndertitelText.Size = new System.Drawing.Size(229, 25);
             this.OndertitelText.TabIndex = 2;
             this.OndertitelText.Text = "Microsoft Dynamics NAV";
-            // 
-            // ServerComboBox
-            // 
-            this.ServerComboBox.Font = new System.Drawing.Font("Arial Unicode MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ServerComboBox.FormattingEnabled = true;
-            this.ServerComboBox.Location = new System.Drawing.Point(170, 116);
-            this.ServerComboBox.Name = "ServerComboBox";
-            this.ServerComboBox.Size = new System.Drawing.Size(200, 26);
-            this.ServerComboBox.TabIndex = 4;
-            this.ServerComboBox.SelectedIndexChanged += new System.EventHandler(this.ServerComboBox_SelectedIndexChanged);
-            // 
-            // ServerLabel
-            // 
-            this.ServerLabel.AutoSize = true;
-            this.ServerLabel.Font = new System.Drawing.Font("Arial Unicode MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ServerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(37)))), ((int)(((byte)(70)))));
-            this.ServerLabel.Location = new System.Drawing.Point(83, 112);
-            this.ServerLabel.Name = "ServerLabel";
-            this.ServerLabel.Size = new System.Drawing.Size(81, 28);
-            this.ServerLabel.TabIndex = 5;
-            this.ServerLabel.Text = "Server:";
             // 
             // pictureBox1
             // 
@@ -107,13 +101,154 @@
             this.NAVImage.TabIndex = 0;
             this.NAVImage.TabStop = false;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rBtnProductie);
+            this.groupBox1.Controls.Add(this.rBtnTest);
+            this.groupBox1.Controls.Add(this.rBtnOntwikkel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 100);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select your server";
+            // 
+            // rBtnProductie
+            // 
+            this.rBtnProductie.AutoSize = true;
+            this.rBtnProductie.Location = new System.Drawing.Point(19, 75);
+            this.rBtnProductie.Name = "rBtnProductie";
+            this.rBtnProductie.Size = new System.Drawing.Size(80, 19);
+            this.rBtnProductie.TabIndex = 9;
+            this.rBtnProductie.Text = "Productie";
+            this.rBtnProductie.UseVisualStyleBackColor = true;
+            this.rBtnProductie.CheckedChanged += new System.EventHandler(this.rBtnProductie_CheckedChanged);
+            // 
+            // rBtnTest
+            // 
+            this.rBtnTest.AutoSize = true;
+            this.rBtnTest.Checked = true;
+            this.rBtnTest.Location = new System.Drawing.Point(19, 52);
+            this.rBtnTest.Name = "rBtnTest";
+            this.rBtnTest.Size = new System.Drawing.Size(51, 19);
+            this.rBtnTest.TabIndex = 8;
+            this.rBtnTest.TabStop = true;
+            this.rBtnTest.Text = "Test";
+            this.rBtnTest.UseVisualStyleBackColor = true;
+            this.rBtnTest.CheckedChanged += new System.EventHandler(this.rBtnTest_CheckedChanged);
+            // 
+            // rBtnOntwikkel
+            // 
+            this.rBtnOntwikkel.AutoSize = true;
+            this.rBtnOntwikkel.Location = new System.Drawing.Point(19, 29);
+            this.rBtnOntwikkel.Name = "rBtnOntwikkel";
+            this.rBtnOntwikkel.Size = new System.Drawing.Size(81, 19);
+            this.rBtnOntwikkel.TabIndex = 7;
+            this.rBtnOntwikkel.Text = "Ontwikkel";
+            this.rBtnOntwikkel.UseVisualStyleBackColor = true;
+            this.rBtnOntwikkel.CheckedChanged += new System.EventHandler(this.rBtnOntwikkel_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbRole);
+            this.groupBox2.Location = new System.Drawing.Point(12, 323);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(460, 100);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select you\'re role";
+            // 
+            // cmbRole
+            // 
+            this.cmbRole.FormattingEnabled = true;
+            this.cmbRole.Location = new System.Drawing.Point(18, 41);
+            this.cmbRole.Name = "cmbRole";
+            this.cmbRole.Size = new System.Drawing.Size(419, 21);
+            this.cmbRole.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rBtnDev);
+            this.groupBox3.Controls.Add(this.rBtnNoExpert);
+            this.groupBox3.Controls.Add(this.rBtnConfig);
+            this.groupBox3.Location = new System.Drawing.Point(12, 429);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(460, 100);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Expert mode";
+            // 
+            // rBtnDev
+            // 
+            this.rBtnDev.AutoSize = true;
+            this.rBtnDev.Location = new System.Drawing.Point(18, 77);
+            this.rBtnDev.Name = "rBtnDev";
+            this.rBtnDev.Size = new System.Drawing.Size(296, 19);
+            this.rBtnDev.TabIndex = 12;
+            this.rBtnDev.Text = "I\'m a hardcore coder and want to be in Dev mode";
+            this.rBtnDev.UseVisualStyleBackColor = true;
+            // 
+            // rBtnNoExpert
+            // 
+            this.rBtnNoExpert.AutoSize = true;
+            this.rBtnNoExpert.Checked = true;
+            this.rBtnNoExpert.Location = new System.Drawing.Point(18, 31);
+            this.rBtnNoExpert.Name = "rBtnNoExpert";
+            this.rBtnNoExpert.Size = new System.Drawing.Size(197, 19);
+            this.rBtnNoExpert.TabIndex = 10;
+            this.rBtnNoExpert.TabStop = true;
+            this.rBtnNoExpert.Text = "Unfortunately, i\'m not an expert";
+            this.rBtnNoExpert.UseVisualStyleBackColor = true;
+            // 
+            // rBtnConfig
+            // 
+            this.rBtnConfig.AutoSize = true;
+            this.rBtnConfig.Location = new System.Drawing.Point(18, 54);
+            this.rBtnConfig.Name = "rBtnConfig";
+            this.rBtnConfig.Size = new System.Drawing.Size(342, 19);
+            this.rBtnConfig.TabIndex = 11;
+            this.rBtnConfig.Text = "I want to Configure everything, i need configuration modus";
+            this.rBtnConfig.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.cmbCompany);
+            this.groupBox4.Location = new System.Drawing.Point(12, 217);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(460, 100);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Select you\'re company";
+            // 
+            // cmbCompany
+            // 
+            this.cmbCompany.FormattingEnabled = true;
+            this.cmbCompany.Location = new System.Drawing.Point(18, 41);
+            this.cmbCompany.Name = "cmbCompany";
+            this.cmbCompany.Size = new System.Drawing.Size(419, 21);
+            this.cmbCompany.TabIndex = 0;
+            this.cmbCompany.SelectedIndexChanged += new System.EventHandler(this.cmbCompany_SelectedIndexChanged);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(397, 541);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 10;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
             // Opstartkeuze
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 450);
-            this.Controls.Add(this.ServerLabel);
-            this.Controls.Add(this.ServerComboBox);
+            this.ClientSize = new System.Drawing.Size(484, 576);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.OndertitelText);
             this.Controls.Add(this.TitleText);
@@ -126,6 +261,12 @@
             this.Text = "Advanced Loginscreen NAV";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.NAVImage)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,8 +278,19 @@
         private System.Windows.Forms.Label TitleText;
         private System.Windows.Forms.Label OndertitelText;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ComboBox ServerComboBox;
-        private System.Windows.Forms.Label ServerLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rBtnProductie;
+        private System.Windows.Forms.RadioButton rBtnTest;
+        private System.Windows.Forms.RadioButton rBtnOntwikkel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbRole;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton rBtnDev;
+        private System.Windows.Forms.RadioButton rBtnNoExpert;
+        private System.Windows.Forms.RadioButton rBtnConfig;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox cmbCompany;
+        private System.Windows.Forms.Button btnOk;
     }
 }
 
--- StartupWizard4PS/Form1.cs
+++ StartupWizard4PS/Form1.cs
@@ -3,75 +3,134 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using StartupWizard4PS.SystemServiceTestRef;
+using StartupWizard4PS.NST100TestContosoProfielOverzichtRef;
 
 namespace StartupWizard4PS
 {
+    //hack: 
+    //<description>
+    //Speciaal voor opstarten 4PS
+    //</description.
+    //Form Opstartkeuze
     public partial class Opstartkeuze : Form
     {
-        public String Server { get; set; }
-        public String Bedrijf { get; set; }
-        public String Rol { get; set; }
+        string cur = "Test";
+        string baseurl; //dkp-appl
+        string NST; //test, prod, ontw (op dit niveau zitten bedrijfen SystemService)
+        string bedrijf; // contoso, sim2 , materieel sim2 (leeg) (hier zitten overige pagina's b.v. ProfielOverzicht)
 
         public Opstartkeuze()
         {
             InitializeComponent();
-            FillServerComboBox();
+            SetCompany("Test");
         }
 
-        private void FillServerComboBox()
+        private void SetCompany(string CompanyName)
         {
-            ServerComboBox.Items.Add("NST100Prod");
-            ServerComboBox.Items.Add("NST100Test");
-            ServerComboBox.Items.Add("NST100Klad");
+            //Default is Test omgeving geselecteerd in radio buttons
+            //Lees dan ook hiervan bedrijven lijst in.
+            //create the SystemService Client
+            baseurl = "http://DKP-APPL-01.gebrdekoning.local:7047";
+            baseurl += ReadCompanyList(CompanyName);
+            cmbCompany.Items.Clear();
+            SystemService_PortClient systemService = new SystemService_PortClient("SystemService_Port", baseurl + "SystemService");
+            string[] companies = systemService.Companies();
+            foreach (string company in companies)
+                cmbCompany.Items.Add(company);
         }
 
-        private void CreateBedrijfKeuze()
+        private string ReadCompanyList(string SelectedCompany)
         {
-            Label BedrijfLabel = new Label
+            switch (SelectedCompany)
             {
-                AutoSize = true,
-                Font = new Font("Arial Unicode MS", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0),
-                ForeColor = Color.FromArgb(27, 37, 70),
-                Location = new Point(83, 147),
-                Name = "BedrijfLabel",
-                Size = new Size(81, 28),
-                Text = "Bedrijf:"
-            };
-            Controls.Add(BedrijfLabel);
+                case "Test":
+                    {
+                        // You can use the parentheses in a case body.
+                        return "/NST100TestWS/WS/";
+                    }
+                case "Ontwikkel":
+                    { 
+                    return "/NST100OntwWS/WS/";
+                    }
+                case "Productie":
+                    {
+                        return "/NST100ProdWS/WS/";
+                    }
+                default:
+                    return "invalid";
+            }
+            
+        }
 
-            ComboBox BedrijfComboBox = new ComboBox
+        private void StartNavision()
+        {
+            //
+            string strCmdText;
+            strCmdText = @"""C:\Program Files (x86)\Microsoft Dynamics NAV\100\RoleTailored Client\Microsoft.Dynamics.Nav.Client.exe"" -settings:""\\DKP-FP-01\APPL\NAV Client Installation 2017\Settings\NST100Test.config"" -profile:""PROJECT MANAGER""";
+            Console.WriteLine(strCmdText);
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+            cmd.StandardInput.WriteLine(strCmdText);
+            Application.Exit();
+        }
+
+        private void cmbCompany_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string baseurl = "http://DKP-APPL-01.gebrdekoning.local:7047/NST100ONTWWS/WS/";
+
+            baseurl = "http://DKP-APPL-01.gebrdekoning.local:7047/NST100TestWS/WS/";
+            //create the ProfielOverzicht Client
+            ProfielOverzicht_PortClient ProfielOverzicht = new ProfielOverzicht_PortClient("ProfielOverzicht_Port", baseurl + Uri.EscapeDataString(cur) + "/Page/ProfielOverzicht");
+
+            ProfielOverzicht_Filter filter1 = new ProfielOverzicht_Filter();
+            filter1.Field = ProfielOverzicht_Fields.Profile_ID;
+            filter1.Criteria = "*";
+
+            ProfielOverzicht_Filter[] filters = new ProfielOverzicht_Filter[] { filter1 };
+            ProfielOverzicht[] Profielen = ProfielOverzicht.ReadMultiple(filters, null, 0);
+            foreach (ProfielOverzicht profiel in Profielen)
             {
-                FormattingEnabled = true,
-                Font = new Font("Arial Unicode MS", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0),
-                Location = new Point(170, 150),
-                Name = "BedrijfComboBox",
-                Size = new Size(200, 26)
-            };
-            Controls.Add(BedrijfComboBox);
-            FillBedrijfComboBox();
+                Console.WriteLine(profiel.Description);
+                if (profiel.Profile_ID != null) cmbRole.Items.Add(profiel.Profile_ID);
+            }
+                
+                
         }
 
-        private void FillBedrijfComboBox()
+        private void btnOk_Click(object sender, EventArgs e)
         {
-           
+            StartNavision();
         }
 
-        private void ResetChoices()
+        private void rBtnOntwikkel_CheckedChanged(object sender, EventArgs e)
         {
-            Server = "";
-            Bedrijf = "";
-            Rol = "";
+            RadioButton rb = sender as RadioButton;
+            if (rb != null) if (rb.Checked) SetCompany("Ontwikkel");
         }
 
-        private void ServerComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        private void rBtnTest_CheckedChanged(object sender, EventArgs e)
         {
-            ResetChoices();
-            Server = ServerComboBox.Text;
-            CreateBedrijfKeuze();
+            RadioButton rb = sender as RadioButton;
+            if (rb != null) if (rb.Checked) SetCompany("Test");
+
+        }
+
+        private void rBtnProductie_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if (rb != null) if (rb.Checked) SetCompany("Productie");
+
         }
     }
 }
